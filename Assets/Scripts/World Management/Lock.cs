544ce937ce49497fdb992d7f03f6d948af297b87
WillJoeLee <github@willjoelee.com>
2022-10-07 16:28:25 -0400

    Approaching a lock will now unlock that quadrant, key logic is next

---
+++ Assets/Scripts/World Management/Lock.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    public GameObject quadrant;
+    public GameObject player;
+    private Transform playerTransform;
+    private bool alreadyUnlocked;
+    private float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      playerTransform = player.transform;
+      alreadyUnlocked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      distance = Vector3.Distance(playerTransform.position, transform.position);
+      Debug.Log(distance);
+      if(distance < 2 && !alreadyUnlocked)
+      {
+        unlock();
+      }
+    }
+
+    private void unlock()
+    {
+      quadrant.SetActive(false);
+      alreadyUnlocked = true;
+    }
+}
