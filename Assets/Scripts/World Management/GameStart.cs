c7a7d4e942c0733e9768b142bc1d23228be59af3
WillJoeLee <github@willjoelee.com>
2022-11-15 17:10:29 -0500

    Added timer that counts down when all players are ready

--- Assets/Scripts/World Management/GameStart.cs
+++ Assets/Scripts/World Management/GameStart.cs
@@ -7,11 +7,18 @@ public class GameStart : MonoBehaviour
 {
     public PlayerInputManager playerInputManager;
     public GameObject ReadyTexts;
+    public TextMesh StartingInTextMesh;
+
+    private bool allReady;
+    private bool countingDown;
+    private float timeEndCountDown;
+    private float timeNow;
 
     // Start is called before the first frame update
     void Start()
     {
-
+      allReady = false;
+      countingDown = false;
     }
 
     // Update is called once per frame
@@ -24,6 +31,29 @@ public class GameStart : MonoBehaviour
       for(int i = 3; i >= playerInputManager.playerCount; i--)
       {
         ReadyTexts.transform.GetChild(i).gameObject.SetActive(false);
+      }
+
+      int numberOfPlayers = playerInputManager.playerCount;
+      int numberOfReadyPlayers = GameObject.FindGameObjectsWithTag("Ready").Length;
+      if((numberOfPlayers == numberOfReadyPlayers) && !countingDown)
+      {
+        timeEndCountDown = Time.realtimeSinceStartup + (float)6;
+        countingDown = true;
+      }
+
+      if(numberOfPlayers != numberOfReadyPlayers)
+      {
+        countingDown = false;
+      }
+
+      if(countingDown)
+      {
+        float timeLeft = timeEndCountDown - Time.realtimeSinceStartup;
+        StartingInTextMesh.text = "Starting in " + (int)timeLeft;
+      }
+      else
+      {
+        StartingInTextMesh.text = "";
       }
     }
 }
