eeb782bf9b2cfed0adb6e080e9f00762bb8bcd57
WillJoeLee <github@willjoelee.com>
2022-11-15 16:44:14 -0500

    Ready text only displays when there is a player connected to it

--- Assets/Scripts/World Management/GameStart.cs
+++ Assets/Scripts/World Management/GameStart.cs
@@ -6,16 +6,24 @@ using UnityEngine.InputSystem;
 public class GameStart : MonoBehaviour
 {
     public PlayerInputManager playerInputManager;
+    public GameObject ReadyTexts;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+      for(int i = 0; i < playerInputManager.playerCount; i++)
+      {
+        ReadyTexts.transform.GetChild(i).gameObject.SetActive(true);
+      }
+      for(int i = 3; i >= playerInputManager.playerCount; i--)
+      {
+        ReadyTexts.transform.GetChild(i).gameObject.SetActive(false);
+      }
     }
 }
