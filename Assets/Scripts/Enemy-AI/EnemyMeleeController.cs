dd5086b5df2adb75fd3bc412c8af83eaf00caf24
Felipe Novaes <felipenovaes@ufl.edu>
2022-11-15 16:49:16 -0500

    added weapon variety

--- Assets/Scripts/Enemy-AI/EnemyMeleeController.cs
+++ Assets/Scripts/Enemy-AI/EnemyMeleeController.cs
@@ -57,7 +57,7 @@ public class EnemyMeleeController : MonoBehaviour
             //collision.gameObject.GetComponent<Animator>().SetTrigger("IsHurt");
             Instantiate(HitParticles, new Vector3(x, y, z), rotation);
 
-            enemy.TakeDamage(Random.Range(5, 45));
+            enemy.TakeDamage(Random.Range(10, 40));
         }
     }
 }
--- Assets/Scripts/Player/MeleeCollisionDetection.cs
+++ Assets/Scripts/Player/MeleeCollisionDetection.cs
@@ -27,7 +27,7 @@ public class MeleeCollisionDetection : MonoBehaviour
             collision.gameObject.GetComponent<Animator>().SetTrigger("IsHurt");
             Instantiate(HitParticles, new Vector3(x, y, z), rotation);
 
-            enemy.TakeDamage(Random.Range(5, 45));
+            enemy.TakeDamage(Random.Range(10, 40));
         }
     }
 }
