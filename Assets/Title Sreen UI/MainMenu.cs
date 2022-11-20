fd32b4e8f6c351af22138a692d2785df591bb7db
Felipe Novaes <felipenovaes@ufl.edu>
2022-11-20 01:26:39 -0500

    fixed main menu scene so that it now successfully loads sample scene again

--- Assets/Title Sreen UI/MainMenu.cs
+++ Assets/Title Sreen UI/MainMenu.cs
@@ -5,9 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    //not in use anymore
     public void PlayGame()
     {
-        Debug.Log("The player selected play game.");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    //update is called once per frame
+    void Update()
+    {
+        //working
+        if (Input.GetMouseButtonDown(0))
+            SceneManager.LoadScene("SampleScene");
+        //not in use yet
+        if (Input.GetMouseButtonDown(1))
+            return;
     }
 }
