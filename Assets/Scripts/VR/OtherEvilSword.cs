79147fd137a671e9dad6c3c34dc74304b9fa4be1
Felipe Novaes <felipenovaes@ufl.edu>
2022-11-29 22:17:55 -0500

    added other sword with enemy spawner

---
+++ Assets/Scripts/VR/OtherEvilSword.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherEvilSword : MonoBehaviour
+{
+    public float swordSpeed = 1f;
+    public float despawnAfterCollide = 5f;
+    public float despawnNoMatterCollide = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Destroy(transform.gameObject, despawnNoMatterCollide);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = transform.position + transform.forward * swordSpeed;
+
+        if (transform.position.y <= 0.5f)
+        {
+            swordSpeed = 0f;
+            transform.GetChild(0).gameObject.SetActive(true);
+            GameObject.Destroy(transform.gameObject, despawnAfterCollide);
+        }
+    }
+}
